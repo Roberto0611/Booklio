afa8cfe6d70bbe4d63b48cddb5002630e0437863
Roberto0611 <ochoacuevasr@gmail.com>
2025-07-02 10:33:42 -0700

    add app.blaed.php to work as a template to all pages

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Booklio</title>
+    @vite('resources/css/app.css')
+  </head>
+<body>
+
+    <header></header>
+
+    @yield('content')
+    
+    <footer></footer>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+</html> 
