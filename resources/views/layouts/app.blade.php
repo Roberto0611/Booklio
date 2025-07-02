ca63b670a23662fae8170d9b305a2e42e9d0cf4f
Roberto0611 <ochoacuevasr@gmail.com>
2025-07-02 11:26:22 -0700

    add Icons and navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,15 +3,19 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="Booklio Icon" href="{{asset('images/logos/book-Icon.svg')}}">
     <title>Booklio</title>
     @vite('resources/css/app.css')
   </head>
 <body>
 
-    <header></header>
+    <header>
+        {{-- nav bar --}}
+         <x-navbar/>
+    </header>
 
     @yield('content')
-    
+
     <footer></footer>
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
