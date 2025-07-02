7d80df158cf6379f23f70a1bd5cd3fbea0ee7b72
Roberto0611 <ochoacuevasr@gmail.com>
2025-07-02 10:03:36 -0700

    Add tailwind

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,1 +1,19 @@
-<h1>Hola mundo</h1>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <h1>hola</h1>
+
+    <div class="p-8 bg-green-500 text-white text-3xl font-bold">
+      ¡Tailwind está funcionando!
+    </div>
+  </body>
+</html>
